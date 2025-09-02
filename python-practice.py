fca1421d684bdeb45ca48f38371d1f4ca70e328d
Mohammed <Barakeh@fmail.com>
2025-09-02 11:04:14 -0400

    played around a bit

--- python-practice.py
+++ python-practice.py
@@ -1,1 +1,7 @@
-print("Hello World")
+Users = ["Mohammed", "Adin"]
+user_name = input("What is your name: ")
+if user_name in Users:
+    print(f"Hello {user_name}, you can access the file")
+else:
+    print(f"Greetings {user_name}, however this is not for you")
+
