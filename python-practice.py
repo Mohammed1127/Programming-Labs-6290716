ed8dd1e0d0732043e060fe55e129cd7cd24ad31c
Mohammed <Barakeh@fmail.com>
2025-09-02 12:03:30 -0400

    changed this file again

--- python-practice.py
+++ python-practice.py
@@ -1,7 +1,9 @@
-Users = ["Mohammed", "Adin"]
+Users = ["Mohammed","Luc"]
 user_name = input("What is your name: ")
 if user_name in Users:
     print(f"Hello {user_name}, you can access the file")
 else:
-    print(f"Greetings {user_name}, however this is not for you")
+    print(f"Greetings {user_name}, this is not for you")
+if user_name not in Users:
+    print("Close the folder immediately like a good little boy")
 
